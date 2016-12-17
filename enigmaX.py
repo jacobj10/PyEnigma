928aca1f69b21ac5ced80aa08b6b081b03c36565
jacobj10 <jacobj10@bbhcsd.org>
2016-12-17 12:33:48 -0500

    TOFIX increment issue

--- enigmaX.py
+++ enigmaX.py
@@ -3,7 +3,7 @@ import os
 gi.require_version('Gtk', '3.0')
 
 from gi.repository import Gtk, GObject, Gdk
-from Scrambler import Scrambler
+from scrambler import Scrambler
 from reflector import Reflector
 
 gtk_builder_file = os.path.splitext(__file__)[0] + '.ui'
@@ -12,21 +12,36 @@ class Engine():
     def __init__(self):
         self.builder = Gtk.Builder()
         self.builder.add_from_file(gtk_builder_file)
-        start1 = 1
-        start2 = 2
-        start3 = 3
+        start1 = 0
+        start2 = 0
+        start3 = 0
         self.scrambler1 = Scrambler(start1)
         self.scrambler2 = Scrambler(start2)
         self.scrambler3 = Scrambler(start3)
         self.reflector = Reflector()
-        self.toString()
+        self.counter = 1
+        print self.run("testfoo")
+        print self.run("RWYRDMM")
 
-    def toString(self):
-        for i in range(0, 26):
-            print ' '.join([self.scrambler1[i], self.scrambler2[i], self.scrambler3[i], 
-                            self.reflector._dict.keys()[i],
-                            self.reflector._dict[self.reflector._dict.keys()[i]],
-                            self.reflector._rev_dict.keys()[i],
-                            self.reflector._rev_dict[self.reflector._rev_dict.keys()[i]]])
-            
+    def translate(self, char):
+        x = self.scrambler1._rev_dict[self.scrambler2._rev_dict[self.scrambler3._rev_dict[self.reflector._dict[self.scrambler3._dict[self.scrambler2._dict[self.scrambler1._dict[char]]]]]]]
+        """
+        self.scrambler1.increment()
+        if self.counter % 26 == 0:
+            self.scrambler2.increment()
+            if self.counter % 52 == 0:
+                self.scrambler3.increment()
+        """
+        return x
+
+    def run(self, plaintext):
+        ciphertext = ''
+        for i in range(0, len(plaintext)):
+            ciphertext += self.translate(plaintext[i].upper())
+            self.counter += 1
+        self.counter = 0
+        #self.scrambler1.reset()
+        #self.scrambler2.reset()
+        #self.scrambler3.reset()
+        return ciphertext
 e = Engine()
--- scrambler.py
+++ scrambler.py
@@ -1,5 +1,6 @@
 class Scrambler():
     def __init__(self, initNum):
+        self.og = initNum
         self.scramblers = []
         self._dict = {}
         self._rev_dict = {}
@@ -10,14 +11,17 @@ class Scrambler():
     def _setup(self, initNum):
         self.scramblers = self.scramblers[initNum:len(self.scramblers)] + self.scramblers[0:initNum]
         for i in range(0, 26):
-            self._dict[chr(i+65)]=chr(self.scramblers[i]+64)
-            self._rev_dict[chr(self.scramblers[i]+64)]=chr(i+65)
+            self._dict[chr(i+65)]=chr(self.scramblers[i]+65)
+            self._rev_dict[chr(self.scramblers[i]+65)]=chr(i+65)
 
     def increment(self):
-        self.scramblers = self.scramblers[1:len(self.scramblers)] + [self.scramblers[0],]
+        self._setup(1)
 
     def letters(self):
         return [chr(letter + 65) for letter in self.scramblers]
 
+    def reset(self):
+        self._setup(self.scramblers.index(self.og))
+        
     def __getitem__(self, n):
         return self.letters()[n]
